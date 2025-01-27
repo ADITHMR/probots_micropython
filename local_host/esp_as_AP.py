f6e405e9835ec0f8bb0fb647fa12940fef29df88
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-01-28 00:22:32 +0530

    x

---
+++ local_host/esp_as_AP.py
@@ -0,0 +1,126 @@
+from local_host.web_page_AP import *
+from file_mgr import *
+from utils import *
+from time import sleep
+from display import *
+
+import json
+try:
+  import usocket as socket
+except:
+  import socket
+
+import network
+import ustruct
+import esp
+esp.osdebug(None)
+import ubinascii
+import gc
+gc.collect()
+
+from oled import *
+
+
+REPLACE_FOR_SPACE="@@!##"
+def enable_AP():
+    oled_log('Access point')
+    oled_log('Please wait...')
+    disp_scroll_str('Access point')
+    
+    
+    sleep(1)
+    
+    
+    
+    ssid = 'Prosol AP'
+    password = '12341234'
+
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid=ssid, password=password)
+    ap.ifconfig(('192.168.1.1', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
+    
+    disp_scroll_str('IP Address -192-168-1-1')
+    
+    sleep(1)
+    while ap.active() == False:
+      pass
+    disp_scroll_str('Conn Success')
+    oled_log('Connect to "Prosol AP"')
+    
+    oled_log('IP->192.168.1.1')
+    print('Connection successful')
+    print(ap.ifconfig())
+
+    
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+        conn, addr = s.accept()
+        print('Got a connection from %s' % str(addr))
+        request = conn.recv(1024)
+        #   print('Content = %s' % str(request))
+        request_str=str(request)
+#         print(request_str)
+      # Check if it's a POST request
+        if "POST" in request_str:
+            # Extract SSID and Password from the request
+            try:
+                start_ssid = request_str.find("ssid=") + 5
+                end_ssid = request_str.find("&", start_ssid)
+                ssid = request_str[start_ssid:end_ssid] if end_ssid != -1 else request_str[start_ssid:]
+
+                start_password = request_str.find("password=") + 9
+                password = request_str[start_password:].split(" ")[0]
+                password=password[:-1]
+
+                ssid=url_decode(ssid)
+                password=url_decode(password)
+                
+                REPLACE_FOR_SPACE
+                if ssid.find(REPLACE_FOR_SPACE) != -1:
+                    ssid=ssid.replace(REPLACE_FOR_SPACE," ")
+                if password.find(REPLACE_FOR_SPACE) != -1:
+                    password=password.replace(REPLACE_FOR_SPACE," ")
+                # Print the received SSID and password
+                print("Received SSID:", ssid)
+                print("Received Password:", password)
+                
+                oled_log('Saved SSID and PASS')
+                set_parameter("SSID",ssid)
+                set_parameter("PASSWORD",password)
+                
+                # Respond with a success message
+                conn.send('HTTP/1.1 200 OK\r\n')
+                conn.send('Content-Type: text/html\r\n')
+                conn.send('\r\n')
+                conn.send("<html><body><h2>Data Received Successfully! Reset the module.</h2></body></html>")
+                conn.close()
+
+            except Exception as e:
+                print("Error:", e)
+                conn.send('HTTP/1.1 400 Bad Request\r\n')
+                conn.send('Content-Type: text/html\r\n')
+                conn.send('\r\n')
+                conn.send("<html><body><h2>Error processing request!</h2></body></html>")
+                
+        else:
+            response = web_page_AP()
+            conn.send(response)
+            conn.close()
+#       if req_data[7:18]=="submit_wifi":
+#           print("success")
+#           data=req_data[24:].split(' ')
+#           data=data[0]
+#           ssid=url_decode(data.split('&')[0])
+#           password=url_decode(data.split('&')[1].split('=')[1])
+#           print("SSID="+str(ssid))
+#           print("Pass="+str(password))
+#           set_parameter("SSID",str(ssid))
+#           set_parameter("PASSWORD",str(password))
+          
+
+   
---
+++ local_host/web_page.py
@@ -0,0 +1,149 @@
+import machine
+from  projectList import  *
+
+
+
+
+# Generate HTML for the dropdown
+html_dropdown =  """<select id="mydropdown" name="selectedItem" class="form-select" required>"""
+for proj in project_topic_list:
+    html_dropdown += f"<option value=\"{proj}\">{proj}</option>\n"
+html_dropdown+="</select>"    
+            
+
+
+
+
+# print(html_dropdown)
+def web_page():
+    with open('local_host/index.html', 'r') as f:
+                html_content = f.read()
+                html_content=html_content.replace("{html_dropdown}",html_dropdown)
+    return html_content
+def successProjectPage(data):
+    success_page=website_style+f"""
+                          <body>
+                            <div class="container"><h1>Project Selection Successful!</h1>
+                            <p>You selected: {data}</p>
+                            
+                            <a>Restart the device.</a>
+                        </div>
+                          </body>
+                        </html>
+                       """
+    return success_page
+def errorPage():
+    error_page=website_style+f"""
+                          <body>
+                            <div class="container"><h1>ERROR!</h1>
+                
+                            
+                            <a href="/reset">Click here to restart the device.</a>
+                        </div>
+                          </body>
+                        </html>
+                       """
+    return error_page
+def restartSuccessPage():
+    error_page=website_style+f"""
+                          <body>
+                            <div class="container"><h1>Restarted Successfully!</h1>
+                        </div>
+                          </body>
+                        </html>
+                       """
+    return error_page
+    
+website_style=f"""
+
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="Control your ESP32 using a web interface." />
+  <title>ESP32 Web Server</title>
+  <meta name="favicon" href="https://img.icons8.com/?size=80&id=dxoYK8bxqiJr&format=png" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+  <style>
+    body {{
+      background-color: #070303;
+      color: #ffffff;
+      font-family: "Roboto", sans-serif;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+    }}
+
+    .container {{
+      display: flex;
+      justify-content: space-between; /* Align form and image side by side */
+      background-color: #1e1e1e;
+      border-radius: 10px;
+      box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+      padding: 20px;
+      width: 90%; /* Adjust the width of the container */
+      flex-wrap: wrap; /* Allow wrapping on small screens */
+    }}
+
+    .form-container {{
+      flex: 1; /* Allow the form to take up the remaining space */
+      margin-right: 20px; /* Add space between the form and image */
+    }}
+
+    .form-container h2 {{
+      color: #03a9f4;
+    }}
+
+    .form-container button {{
+      font-size: 18px;
+      padding: 12px 24px;
+      margin: 5px;
+    }}
+
+    .form-container button.btn-success {{
+      background-color: #4caf50;
+    }}
+
+    .form-container button.btn-danger {{
+      background-color: #f44336;
+    }}
+
+    .form-container button.btn-warning {{
+      background-color: #ff9800;
+    }}
+
+    .form-container p {{
+      font-size: 20px;
+      color: #ccc;
+    }}
+
+    /* Responsive image */
+    #image {{
+      max-width: 100%; /* Image width */
+      max-height: 60vh; /* Make image responsive to the height of the viewport */
+      height: auto;
+      border-radius: 10px;
+    }}
+
+    /* Media query for mobile screens */
+    @media (max-width: 768px) {{
+      .container {{
+        flex-direction: column; /* Stack form and image vertically on smaller screens */
+        align-items: center;
+      }}
+
+      .form-container {{
+        margin-right: 0; /* Remove space between form and image on small screens */
+        margin-bottom: 20px; /* Add some space below the form */
+      }}
+    }}
+  </style>
+</head>
+  """
