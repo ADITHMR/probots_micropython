9dbde952c7e545f812c19b29c8fdb806601f4b33
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-01-31 15:49:37 +0530

    bot updated

--- boot.py
+++ boot.py
@@ -1,13 +1,16 @@
+import time
 
-from local_host.esp_as_AP import enable_AP
+
 from pin_mapping import TOUCH1,TOUCH2
-from local_host.webServer import runWebServer
+
 
 if TOUCH1.value()==True and TOUCH2.value()==True:
+    from local_host.esp_as_AP import enable_AP
     enable_AP()
 elif  TOUCH1.value()==True and TOUCH2.value()==False:
+    from local_host.webServer import runWebServer
     runWebServer()
-    
+time.sleep(1)
 from process.save_html import save_html
 from process.fetch_projects import fetch_projects
 
