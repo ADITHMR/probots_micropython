9692292e3a8100ac7d53caec08a01cdf45e424f0
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-02-05 23:00:20 +0530

    boot display update

--- boot.py
+++ boot.py
@@ -3,17 +3,18 @@ import time
 
 from pin_mapping import TOUCH1,TOUCH2
 from local_host.connect_wifi import  connect_wifi
+from drivers.oled import *
+oled_two_data(1,1,"System","Booting")
 # from machine_id import get_update_flag
-
+time.sleep(.5)
 
 if TOUCH1.value()==True and TOUCH2.value()==True:
     from local_host.esp_as_AP import enable_AP
     enable_AP()
 elif  TOUCH1.value()==True and TOUCH2.value()==False:
     from local_host.webServer import runWebServer
-    from drivers.oled import oled_log
-    oled_log("Web server ")
-    oled_log("Mode ")
+    oled_two_data(1,1,"Web server","Mode")
+    time.sleep(.5)
     runWebServer()
 
 connect_wifi()
@@ -24,24 +25,25 @@ temp=str(data["update_flag"])
 print(f"update_status={temp}")
 if temp=="True":
     print("Starting Update")
+    time.sleep(.5)
     try:
         from drivers.oled import oled_log
-        oled_two_data(1,3,"Starting","Update")
+        oled_two_data(1,1,"Starting","Update")
     except Exception as e:
         print("OLED Error:", e)
     from ota_update import run_update
     run_update()
-    
-    
-time.sleep(1)
 
+time.sleep(.5)
 from process.save_html import save_html
 from process.fetch_projects import fetch_projects
 
 
-
-
-
-
+oled_two_data(1,1,"Setting up","Activities")
+time.sleep(.5)
 fetch_projects()
+oled_two_data(1,1,"Updating","Files")
+time.sleep(.5)
 save_html()
+oled_two_data(1,1,"Starting","Activities")
+time.sleep(.5)
