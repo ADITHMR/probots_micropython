575aa7c84d79f711e1e8f68a69b519f7f3ec2141
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-02-17 20:20:23 +0530

    x

--- lib/custom_neopixel.py
+++ lib/custom_neopixel.py
@@ -5,8 +5,9 @@ class CustomNeoPixel:
     def __init__(self, pin, num_pixels, enabled=True):
         """Initializes the NeoPixel strip."""
         self.is_enabled = enabled
+        self.num_pixels=num_pixels
         if self.is_enabled:
-            self.np = neopixel.NeoPixel(machine.Pin(pin,machine.Pin.OUT), num_pixels)
+            self.np = neopixel.NeoPixel(machine.Pin(pin,machine.Pin.OUT), self.num_pixels)
 
     def set_color(self, index, r, g, b):
         """Sets the color of a specific NeoPixel."""
@@ -21,3 +22,8 @@ class CustomNeoPixel:
             self.np.fill((0, 0, 0))  # More efficient to fill all pixels at once
             self.np.write()
 
+    def set_color_All(self,r,g,b):
+        """Turns off all the NeoPixels."""
+        if self.is_enabled:
+            for k in range(self.num_pixels):
+                self.set_color(k,r,g,b)
