78f5b563942ef4f238f678b55be887f5278e2240
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-02-22 22:18:14 +0530

    x

--- boot.py
+++ boot.py
@@ -1,31 +1,40 @@
-import time
-from machine import Pin
+try:
+    from drivers.oled import *
+    oled_two_data(1,1,"System","Booting")
+    import time
+    from machine import Pin
+    from local_host.connect_wifi import  connect_wifi
+    import process.api as api
+    from local_host.esp_as_AP import runAP
+    from local_host.webServer import runWebServer
+    import json
 
+    from ota_update import run_update
+    from process.save_html import save_html
+except Exception as e:
+        print("Error boot() import files error:",e, e.args)
 
-
-
-from local_host.connect_wifi import  connect_wifi
-from drivers.oled import *
-oled_two_data(1,1,"System","Booting")
 # from machine_id import get_update_flag
 # time.sleep(.5)
 TOUCH1=Pin(14,Pin.IN)
 TOUCH2=Pin(15,Pin.IN)
 if TOUCH1.value()==True and TOUCH2.value()==True:
-    from local_host.esp_as_AP import runAP
-    runAP()
+    try:
+        runAP()
+    except Exception as e:
+        print("Error Boot() RunAP:",e, e.args)
 elif  TOUCH1.value()==True and TOUCH2.value()==False:
     oled_two_data(1,1,"Web server","Mode")
     wifi_connected=connect_wifi()
     if wifi_connected:
         oled_log("Web Server")
-        from local_host.webServer import runWebServer
+        
         
         time.sleep(1)
         runWebServer()
 
 wifi_connected=connect_wifi()
-import json
+
 if wifi_connected:
     with open('schema.dat', 'r') as f:
         data = json.load(f)
@@ -35,14 +44,14 @@ if wifi_connected:
         print("Starting Update")
     #     time.sleep(.5)
         try:
-            from drivers.oled import oled_log
+            
             oled_two_data(1,1,"Starting","Update")
         except Exception as e:
             print("OLED Error:", e)
-        from ota_update import run_update
+        
         run_update()
 if wifi_connected:
-    import process.api as api
+    
     api=api.Api()
     login=api.user_login()
     if login:
@@ -55,7 +64,7 @@ if wifi_connected:
         oled_two_data(1,1,"Login","Failed")
 
 # time.sleep(.5)
-from process.save_html import save_html
+
 
 
 
