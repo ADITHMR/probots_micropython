281cdc5d6d4012d241caa63755b41e9edd1b7b10
ADITHMR <79104481+ADITHMR@users.noreply.github.com>
2025-02-23 15:01:49 +0530

    x

--- process/run_activity.py
+++ process/run_activity.py
@@ -1,8 +1,23 @@
-from activity1.pactivity import run_activity as run_activity1
-from activity2.pactivity import run_activity as run_activity2
-from activity3.pactivity import run_activity as run_activity3
-from activity4.pactivity import run_activity as run_activity4
-from activity5.pactivity import run_activity as run_activity5
+try:
+    from activity1.pactivity import run_activity as run_activity1
+except:
+    run_activity1=0
+try:
+    from activity2.pactivity import run_activity as run_activity2
+except:
+    run_activity2=0
+try:
+    from activity3.pactivity import run_activity as run_activity3
+except:
+    run_activity3=0
+try:
+    from activity4.pactivity import run_activity as run_activity4
+except:
+    run_activity4=0
+try:
+    from activity5.pactivity import run_activity as run_activity5
+except:
+    run_activity5=0
 from process.route_activity import route_activity
 def run(project_name):
 	A=route_activity(project_name)
